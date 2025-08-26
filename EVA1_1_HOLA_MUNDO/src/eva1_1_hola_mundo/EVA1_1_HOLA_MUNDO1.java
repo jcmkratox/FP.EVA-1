9bd6c7f83d3cd3482831423f414cc88b20eeb475
jcmkratox <L25550589@chihuahua2.tecnm.mx>
2025-08-26 18:57:05 -0500

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author jcmkr
+ */
+public class EVA1_1_HOLA_MUNDO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo  ");
+        System.out.println("fundamentos de programacion");     // TODO code application logic here
+    }
+    
+}

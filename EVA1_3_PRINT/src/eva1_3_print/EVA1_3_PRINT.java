655335bd442540be62ceca012c04ea57656afa7b
jcmkratox <L25550589@chihuahua2.tecnm.mx>
2025-08-27 18:00:17 -0500

    Add files via upload

---
+++ EVA1_3_PRINT/src/eva1_3_print/EVA1_3_PRINT.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_print;
+
+/**
+ *
+ * @author jcmkr
+ */
+public class EVA1_3_PRINT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("NOMBRE DEL ESTUDIANTE: ");
+        System.out.println("Joaquin carbajal");
+        System.out.print("NUMERO DE CONTROL: ");
+        System.out.println("25550589");
+        System.out.print("EDAD: ");
+        System.out.println(18 );
+        // TODO code application logic here
+    }
+    
+}

9f9572c9489f43fccc24cd4568e94bcec317d0f6
jcmkratox <L25550589@chihuahua2.tecnm.mx>
2025-08-27 22:52:44 -0500

    Add files via upload

---
+++ EVA_1_4_EMPLEO/src/eva_1_3_empleo/EVA_1_3_EMPLEO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_3_empleo;
+
+/**
+ *
+ * @author jcmkr
+ */
+public class EVA_1_3_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("EMPLEO: " );
+        System.out.println("cajero");
+        System.out.print("Salario: ");
+        System.out.println("1200");
+        System.out.print("Hora semana: ");
+        System.out.println("20");
+        System.out.print("requiere ingles: ");
+        System.out.println("si");
+        // TODO code application logic here
+    }
+    
+}

8b942a14b4820a0317908539e554a138ec8e9a89
jcmkratox <L25550589@chihuahua2.tecnm.mx>
2025-08-27 22:53:41 -0500

    Add files via upload

---
+++ EVA_1_5_VAREABLES/src/eva_1_5_vareables/EVA_1_5_VAREABLES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_5_vareables;
+
+/**
+ *
+ * @author jcmkr
+ */
+public class EVA_1_5_VAREABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARACION DE VARIABLES
+        //crearlas las variables paar que se pueda usar
+        //tipo de dato  indentifiado 
+        String nombre;
+        byte edad;
+        //INICIACION
+        //usamos el operador = :operador de asignacion
+        //PONE "JOAQUIN CARBAJAL" DENTRO DE LA VARIABLE NOMBRE 
+        nombre = "joaquin carbajal";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+        // TODO code application logic here
+    }
+    
+}

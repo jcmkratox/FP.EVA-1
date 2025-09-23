6a703f35d6674f0eeee70047024a963686f16b66
jcmkratox <L25550589@chihuahua2.tecnm.mx>
2025-09-22 23:11:39 -0500

    Add files via upload

---
+++ EVA_1_18_MESES/src/eva_1_18_meses/EVA_1_18_MESES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_18_meses;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcmkr
+ */
+public class EVA_1_18_MESES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int mes;
+     Scanner captu = new Scanner (System.in);
+     System.out.println("NUMERO DEL DIA DEL MES");
+     mes = captu.nextInt();
+     if(mes == 1 );{
+     System.out.println("enero ");
+    }else if(mes == 2);{}
+    
+   
+     
+    }         
+}
